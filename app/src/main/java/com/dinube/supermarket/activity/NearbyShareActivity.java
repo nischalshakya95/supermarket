7ae7eb95566a10d38c2a0588445b2aee388c0503
nischalshakya95 <nischalshakya@lftechnology.com>
2020-09-21 17:15:31 +0545

    Updating code

--- app/src/main/java/com/dinube/supermarket/activity/NearbyShareActivity.java
+++ app/src/main/java/com/dinube/supermarket/activity/NearbyShareActivity.java
@@ -6,10 +6,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.widget.Switch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dinube.supermarket.R;
 import com.dinube.supermarket.nearbyshare.NearbyAdvertise;
+import com.dinube.supermarket.utils.UiUtils;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
 
 public class NearbyShareActivity extends AppCompatActivity {
 
@@ -37,5 +42,20 @@ public class NearbyShareActivity extends AppCompatActivity {
                 NearbyAdvertise.stopAdvertising(context, endpointName);
             }
         });
+    }
+
+    class NearbyPayloadCallback extends PayloadCallback {
+
+        @Override
+        public void onPayloadReceived(@NonNull String s, @NonNull Payload payload) {
+            byte[] receivedBytes = payload.asBytes();
+            String message = new String(receivedBytes);
+            UiUtils.showToast(context, message);
+        }
+
+        @Override
+        public void onPayloadTransferUpdate(@NonNull String s, @NonNull PayloadTransferUpdate payloadTransferUpdate) {
+
+        }
     }
 }
